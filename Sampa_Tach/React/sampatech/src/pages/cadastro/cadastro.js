ac47a0d4c51b3e3f34b1bf942aa89ac3c8b73a0b
unknown <ggnespoli@gmail.com>
2021-08-06 17:22:31 -0300

    front melhor q back

--- Sampa_Tach/React/sampatech/src/pages/cadastro/cadastro.js
+++ Sampa_Tach/React/sampatech/src/pages/cadastro/cadastro.js
@@ -1,4 +1,6 @@
 import { Component } from "react";
+import './cadastro.css'
+import logo from '../../img/logo1.png'
 
 class Cadastro extends Component
 {
@@ -6,14 +8,87 @@ class Cadastro extends Component
     {
         super(props);
         this.state={
-
+            userName    : '',
+            email       : '',
+            senha       : '',
         }
+    }
+
+    atualizarState = (state) => {
+        this.setState({ [state.target.name] : state.target.value})
+    }
+
+    cadastro = (event) =>{
+        event.preventDefault()
+
+        fetch('localhost',{
+            method : 'POST',
+
+            body : JSON.stringify({
+                userName    : this.state.userName,
+                email       : this.state.email,
+                senha       : this.state.senha    
+            }),
+        })
+
+        .then(resp => {
+
+            if (resp.status === 201) {
+                this.props.history.push('/')
+            }
+        })
     }
 
     render()
     {
         return(
-            <p>cadastro</p>
+            <main className="main-cd">
+            <div className="barraLateral-cd"></div>
+            <section className="section-cd">
+                <div className="header-cd">
+                    <p>cadastro</p>
+                </div>
+    
+                <div className="login-cd">
+    
+                    <div className="meio0-cd">
+                        <img src={logo} alt=""/>
+                    </div>
+    
+                    <div className="meio1-cd">
+                        <div className="linha-cd"></div>
+                    </div>
+    
+                    <div className="meio2-cd">
+    
+                        <div className="forms-cd">
+    
+                            <form className="form-cd">
+                                <div className="metadeForm-cd">
+    
+                                    <input className="input-cd" type="text" placeholder="Nome"/>
+    
+                                    <input className="input-cd" type="text" placeholder="email"/>
+        
+                                    <input className="input-cd" type="text" placeholder="senha"/>
+                                </div>
+    
+                                <div className="btn-cd">
+                                    
+                                    <button className="button-cd" type="submit">cadastro</button>
+    
+                                </div>
+                            </form>
+    
+                        </div>
+    
+    
+                    </div>
+    
+                </div>
+                
+            </section>
+        </main>
         )
     }
 }
--- Sampa_Tach/React/sampatech/src/pages/login/login.js
+++ Sampa_Tach/React/sampatech/src/pages/login/login.js
@@ -1,4 +1,10 @@
+import axios from "axios";
 import { Component } from "react";
+import { parseJWT } from "../../services/auth";
+
+import "./login.css";
+
+import logo from "../../img/logo1.png";
 
 class Login extends Component
 {
@@ -6,14 +12,101 @@ class Login extends Component
     {
         super(props);
         this.state={
-
+            email           : '',
+            senha           : '',
+            errormensage    : '',
         }
+    }
+
+    efetuarlogin = (event) => {
+        event.preventDefault()
+
+        this.setState({errormensage : ''})
+
+        axios.post('localhost',
+        {
+            email : this.state.email,
+            senha : this.state.senha
+        })
+
+        .then(resp => {
+            if (resp.status === 200) {
+
+                localStorage.setItem('usuario-login', resp.data.token)
+
+                if (parseJWT().role === 1) {
+                    this.props.history.push('/adm')
+                }
+
+                if (parseJWT().role === 2) {
+                    this.props.history.push('/salas')
+                }
+            }
+        })
+
+        .catch( () => {
+            this.setState({ errorMensage : "Email ou senha inválidos!! tente novamente"})
+        })
+        
+    }
+
+    atualizarState = (state) => 
+    {
+        this.setState({ [state.target.name] : state.target.value })
+
+        console.log(this.state.senha)
+        console.log(this.state.email)
     }
 
     render()
     {
         return(
-            <p>login</p>
+        <main className="main-lg">
+            <div className="barraLateral-lg"></div>
+            <section className="section-lg">
+                <div className="header-lg">
+                    <p>login</p>
+                </div>
+    
+                <div className="login-lg">
+    
+                    <div className="meio0-lg">
+                        <img src={logo} alt=""/>
+                    </div>
+    
+                    <div className="meio1-lg">
+                        <div className="linha-lg"></div>
+                    </div>
+    
+                    <div className="meio2-lg">
+    
+                        <div className="forms-lg">
+    
+                            <form className="form-lg" onSubmit={this.efetuarlogin}>
+                                <div className="metadeForm-lg">
+    
+                                    <input className="input-lg" type="text" placeholder="email" name="email" value={this.state.email} onChange={this.atualizarState}/>
+        
+                                    <input className="input-lg" type="text" placeholder="senha" name="senha" value={this.state.senha} onChange={this.atualizarState}/>
+                                </div>
+    
+                                <div className="btn-lg">
+                                    
+                                    <button type="submit">logar</button>
+
+                                    <p className="btn-p-lg">{this.state.errormensage}</p>
+    
+                                </div>
+                            </form>
+    
+                        </div>
+    
+                    </div>
+    
+                </div>
+                
+            </section>
+        </main>
         )
     }
 }
